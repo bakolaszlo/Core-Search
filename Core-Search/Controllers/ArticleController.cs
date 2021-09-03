ecd7c12133b7f3a1d119625f11068f4ac428876a
ionutroth <87963655+ionutroth@users.noreply.github.com>
2021-09-03 19:33:06 +0300

    Update ArticleController.cs

--- Core-Search/Controllers/ArticleController.cs
+++ Core-Search/Controllers/ArticleController.cs
@@ -19,27 +19,29 @@ namespace Core_Search.Controllers
         [HttpGet]
         public IEnumerable<Article> Get(string searchField)
         {
-            //var testing = "https://core.ac.uk:443/api-v2/search/{searchField}?page=1&pageSize=10&apiKey=BMnmqU7poFJc5HVLYNOwdGkE9QiR0DxI";
             var client = new RestClient($"https://core.ac.uk:443/api-v2/search/{searchField}?page=1&pageSize=10&apiKey=BMnmqU7poFJc5HVLYNOwdGkE9QiR0DxI");
             client.Timeout = -1;
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
+
             return ConvertResponseToArticle(response.Content);
-            var testconsole = ConvertResponseToArticle(response.Content);
-            Console.WriteLine("ceva");
         }
 
         public IEnumerable<Article> ConvertResponseToArticle(string content, int articlesNumber = 9)
         {
             var json = JObject.Parse(content)["data"];
 
-            return Enumerable.Range(1, articlesNumber).Select(index => new Article
+            return Enumerable.Range(1, articlesNumber).Select(index =>
             {
-                Id = json[index]["_source"].Value<int>("id"),
-                Author = json[index]["_source"].Value<string>("publisher"),
-                Title = json[index]["_source"].Value<string>("title"),
-                Description = json[index]["_source"].Value<string>("description"),
-                Link = json[index]["_source"].Value<string>("downloadUrl")
+                var JsonSource = json[index]["_source"];
+                return new Article
+                {
+                    Id = JsonSource.Value<int>("id"),
+                    Author = JsonSource.Value<string>("publisher"),
+                    Title = JsonSource.Value<string>("title"),
+                    Description = JsonSource.Value<string>("description"),
+                    Link = JsonSource.Value<string>("downloadUrl")
+                };
             }).ToArray();
         }
     }
